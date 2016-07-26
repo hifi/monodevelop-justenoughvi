fee3268b423a156846d487d56c2224f8fad00645
Toni Spets <toni.spets@iki.fi>
2016-07-26 19:34:58 +0300

    Bump dev version to v0.12

--- JustEnoughVi/Properties/AddinInfo.cs
+++ JustEnoughVi/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "JustEnoughVi",
     Namespace = "JustEnoughVi",
-    Version = "0.11"
+    Version = "0.12"
 )]
 
 [assembly: AddinName("Just Enough Vi")]
